d8582ee92ce6e63be7ac7a46398cebaaa260017e
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-08-01 22:18:38 -0700

    10%

--- week3/creating-a-server/server.js
+++ week3/creating-a-server/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 
 // telling app to listen to port with callback function showing it is in fact working.
-app.listen(6000, () => {
-  console.log("App is listening on port 6000!");
+app.listen(3000, () => {
+  console.log("App is listening on port 3000!");
 });
-// using 6000 rather than 3000 because I have 3000 already in use on my machine and can't find where that 
-// server is running
